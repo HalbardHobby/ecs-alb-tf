e7c8eb23dc7f43b305e02f63fad6632c6d2d515a
Alejandro <HalbardHobby@users.noreply.github.com>
2023-03-21 13:00:52 -0500

    ✨ added alb target group

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -29,3 +29,36 @@ resource "aws_security_group" "alb_security_group" {
     Name = "alb-securrity-group"
   }
 }
+
+resource "aws_lb_target_group" "target_group" {
+  name = "alb-target-group"
+  port = 80
+  protocol = "HTTP"
+  target_type = "ip"
+  vpc_id = aws_vpc.main.id
+
+  health_check {
+    healthy_threshold = "3"
+    interval = "300"
+    protocol = "HTTP"
+    matcher = "200"
+    timeout = "3"
+    path = "/v1/status"
+    unhealthy_threshold = "2"
+  }
+
+  tags = {
+    "Name" = "alb-target-group"
+  }
+}
+
+resource "aws_alb_listener" "listener" {
+  load_balancer_arn = aws_lb.application_load_balancer.id
+  port = "80"
+  protocol = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.target_group.id
+  }
+}
